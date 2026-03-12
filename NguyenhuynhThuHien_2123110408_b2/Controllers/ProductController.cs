ac93d19b7cb151884f1e816f357a774f9ffee66f
NguyenHuynhThuHien <thuhien.bae@gmail.com>
2026-03-12 07:58:02 +0700

    Add project files.

---
+++ NguyenhuynhThuHien_2123110408_b2/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NguyenhuynhThuHien_2123110408_b2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+    }
+}
