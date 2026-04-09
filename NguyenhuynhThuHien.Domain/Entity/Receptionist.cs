be44fc441fe09f8614e04ab7ad3163d8e938c183
NguyenHuynhThuHien <thuhien.bae@gmail.com>
2026-04-09 09:17:11 +0700

    Add Receptionist class to manage receptionist details
    Introduced a new `Receptionist` class in the
    `NguyenhuynhThuHien.Domain.Entity` namespace. The class
    includes properties for `Id`, `Name`, `Phone`, and a
    foreign key `UserId` to link to a `User` object,
    facilitating the management of receptionist information.

---
+++ NguyenhuynhThuHien.Domain/Entity/Receptionist.cs
@@ -0,0 +1,13 @@
+﻿namespace NguyenhuynhThuHien.Domain.Entity
+{
+    public class Receptionist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; } // Thêm SĐT để quản lý
+
+        // Khóa ngoại liên kết với bảng User
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
